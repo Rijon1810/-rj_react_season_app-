cc69a518a51786c15ac4b8ff4df4727600691c48
Rijon <rijon.r@yahoo.com>
2021-02-10 20:24:29 +0600

    make it better

--- src/index.js
+++ src/index.js
@@ -2,48 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner'
-class App extends React.Component{
-     //initialization
-    state = { late : null , errorMessage : ''};
+import useLocation from  './useLocation';
 
-    componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude}),
-            err =>  this.setState({errorMessage: err.message })
-        );
-    }
 
-    componentDidUpdate(){
-        console.log('My component was just updated - it rerendered');
-    }
+const App = () => {
+       const [lat , errorMessage] = useLocation();
 
-    
-    renderContent(){
-        //conditional rendering
-        if(this.state.errorMessage && !this.state.lat)
-        {
-            return  <div>Error : {this.state.errorMessage}</div>
-        }
-        else if(!this.state.errorMessage && this.state.lat){
-            return <SeasonDisplay lat={this.state.lat}/>
-        }
-        else{
-            return <Spinner message="Please accept location request!!!!"/>
-        }
+     let content;
+     if(errorMessage){
+         content = <div>Error : {errorMessage}</div>;
+     }
+     else if(lat){
+          content = <SeasonDisplay lat={lat}/>
+     }
+     else{
+         content = <Spinner message="Please accept location request!!!!"/>;
+     }
 
-    }
+     return <div className="border red">{content}</div>
+};
 
-  //React says we have to define render !!!
-    render(){
-        return(
-            <div className="border red">
-                {this.renderContent()}
-            </div>
-
-        );
-        
-
-    }
-}
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
